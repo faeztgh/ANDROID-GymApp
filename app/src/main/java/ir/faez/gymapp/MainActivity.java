ade5009d9c3292e85fff8df7d5ffdeeeaa259ae0
faez <faeztgh@users.noreply.github.com>
2021-02-05 19:51:58 +0330

    make items and allcourses and myCourses layout

--- app/src/main/java/ir/faez/gymapp/MainActivity.java
+++ app/src/main/java/ir/faez/gymapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_signup);
     }
 }
